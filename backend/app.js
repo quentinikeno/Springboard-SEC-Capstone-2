0962cea07faf3f142c5e370868977bea41e7c71d
Quentin Ikeno <ikenoq@gmail.com>
2022-10-16 22:00:32 -1000

    Install helmet.

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ const express = require("express");
 const app = express();
 const ExpressError = require("./expressError");
 const morgan = require("morgan");
-const userRoutes = require("./routes/user");
+const helmet = require("helmet");
 
+/** Middleware */
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(helmet());
 
 //** Routes */
+const userRoutes = require("./routes/user");
 app.use("/user", userRoutes);
 
 /** 404 handler */
