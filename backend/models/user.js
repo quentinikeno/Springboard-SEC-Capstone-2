b6dba756cf6e90804d3cb4669b50511e319bc2b1
Quentin Ikeno <ikenoq@gmail.com>
2022-10-17 18:41:47 -1000

    Add method to register.

--- backend/models/user.js
+++ backend/models/user.js
@@ -13,15 +13,34 @@ class User {
 		this.lastLoginAt = lastLoginAt;
 	}
 
-	/** return all users
-	 * => [id, username, joinAt, lastLoginAt}, {id, username, joinAt, lastLoginAt}, ...]
+	/** register new user -- returns
+	 *    {id, username, joinAt, lastLoginAt}
+	 */
+
+	static async register({ username, email, password }) {
+		//Add json Schema later
+		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR); // hash password
+		const results = await db.query(
+			`
+        INSERT INTO users (username, password, email, join_at, last_login_at)
+        VALUES ($1, $2, $3, current_timestamp, current_timestamp)
+        RETURNING id, username, join_at AS "joinAt", last_login_at AS "lastLoginAt"`,
+			[username, hashedPassword, email]
+		);
+		return results.rows[0];
+	}
+
+	/** get all users
+	 * returns [id, username, joinAt, lastLoginAt}, {id, username, joinAt, lastLoginAt}, ...]
 	 */
 	static async getAllUsers() {
 		const results = await db.query(`
-        SELECT id, username, join_at AS "joinAt", last_login_at AS lastLoginAt
+        SELECT id, username, join_at AS "joinAt", last_login_at AS "lastLoginAt"
         FROM users
         ORDER BY id
         `);
 		return results.rows.map((user) => new User(user));
 	}
 }
+
+module.exports = User;
