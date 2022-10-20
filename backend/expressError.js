17246d6be5b173ad71ad9b29c166212c8e810c4e
Quentin Ikeno <ikenoq@gmail.com>
2022-10-19 16:58:36 -1000

    Extend the ExpressError with addtional error codes.

--- backend/expressError.js
+++ backend/expressError.js
@@ -1,6 +1,5 @@
 class ExpressError extends Error {
-	/** This class let's us customize our errors for the API.
-	 * Used as part of the error-handling middleware.*/
+	/** This class let's us customize our errors for the API.*/
 	constructor(message, status) {
 		super();
 		this.message = message;
@@ -9,4 +8,44 @@ class ExpressError extends Error {
 	}
 }
 
-module.exports = ExpressError;
+/** Additional errors */
+
+/** 404 NOT FOUND error. */
+
+class NotFoundError404 extends ExpressError {
+	constructor(message = "Not Found") {
+		super(message, 404);
+	}
+}
+
+/** 401 UNAUTHORIZED error. */
+
+class UnauthorizedError401 extends ExpressError {
+	constructor(message = "Unauthorized") {
+		super(message, 401);
+	}
+}
+
+/** 400 BAD REQUEST error. */
+
+class BadRequestError400 extends ExpressError {
+	constructor(message = "Bad Request") {
+		super(message, 400);
+	}
+}
+
+/** 403 BAD REQUEST error. */
+
+class ForbiddenError403 extends ExpressError {
+	constructor(message = "Bad Request") {
+		super(message, 403);
+	}
+}
+
+module.exports = {
+	ExpressError,
+	NotFoundError404,
+	UnauthorizedError401,
+	BadRequestError400,
+	ForbiddenError403,
+};
