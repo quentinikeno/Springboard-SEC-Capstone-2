33afee3e7ffabcc3f38d91bf043baef6c00b1dee
Quentin Ikeno <ikenoq@gmail.com>
2022-10-19 18:04:12 -1000

    Create helper to sign JWT.

---
+++ backend/helpers/createToken.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
+/** Create a JWT from the user's username and sign it. */
+
+function createToken(user) {
+	// pass in user instance
+	const payload = { username: user.username };
+	// return signed JWT
+	return jwt.sign(payload, SECRET_KEY);
+}
+
+module.exports = createToken;
