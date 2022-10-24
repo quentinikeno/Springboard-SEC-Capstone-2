f3642cec562e6b273f1d98f14e902fa7a20ee2fa
Quentin Ikeno <ikenoq@gmail.com>
2022-10-23 17:21:36 -1000

    Add tests for PATCH /user/[username].

--- backend/tests/userRoutes.test.js
+++ backend/tests/userRoutes.test.js
@@ -15,10 +15,10 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
-const GetU1Token = async () => {
+const GetUserToken = async (username = "user1", password = "password1") => {
 	const res = await request(app).post("/auth/login").send({
-		username: "user1",
-		password: "password1",
+		username,
+		password,
 	});
 	return res.body.token;
 };
@@ -27,7 +27,7 @@ const GetU1Token = async () => {
 
 describe("test GET /user/[username]", () => {
 	it("can get info on a certain user", async () => {
-		const u1Token = await GetU1Token();
+		const u1Token = await GetUserToken();
 		const resp = await request(app)
 			.get("/user/user1")
 			.set("authorization", `Bearer ${u1Token}`);
@@ -38,5 +38,109 @@ describe("test GET /user/[username]", () => {
 			lastLoginAt: expect.any(String),
 			joinAt: expect.any(String),
 		});
+	});
+
+	it("thows a 404 status code if the user doesn't exist", async () => {
+		const u1Token = await GetUserToken();
+		const resp = await request(app)
+			.get("/user/nonexistant")
+			.set("authorization", `Bearer ${u1Token}`);
+
+		expect(resp.statusCode).toBe(404);
+	});
+});
+
+describe("test PATCH /user/[username]", () => {
+	it("can update info on a certain user", async () => {
+		const u1Token = await GetUserToken();
+		const resp = await request(app)
+			.patch("/user/user1")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({
+				email: "wow@mail.com",
+				newPassword: "superSecret",
+				oldPassword: "password1",
+				username: "betterUsername",
+			});
+
+		expect(resp.body).toEqual({
+			id: expect.any(Number),
+			username: "betterUsername",
+			lastLoginAt: expect.any(String),
+			joinAt: expect.any(String),
+		});
+
+		// check that we can access the get route with the new username
+		const updatedU1Token = await GetUserToken(
+			"betterUsername",
+			"superSecret"
+		);
+		const resp2 = await request(app)
+			.get("/user/betterUsername")
+			.set("authorization", `Bearer ${updatedU1Token}`);
+
+		expect(resp2.body).toEqual({
+			id: expect.any(Number),
+			username: "betterUsername",
+			lastLoginAt: expect.any(String),
+			joinAt: expect.any(String),
+		});
+	});
+
+	it("can update part of a user's data", async () => {
+		const u1Token = await GetUserToken();
+		const resp = await request(app)
+			.patch("/user/user1")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({
+				email: "wow@mail.com",
+				oldPassword: "password1",
+			});
+
+		expect(resp.body).toEqual({
+			id: expect.any(Number),
+			username: "user1",
+			lastLoginAt: expect.any(String),
+			joinAt: expect.any(String),
+		});
+	});
+
+	it("throws an error if the password is wrong or not included", async () => {
+		const u1Token = await GetUserToken();
+		const resp = await request(app)
+			.patch("/user/user1")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({
+				email: "wow@mail.com",
+				newPassword: "superSecret",
+				oldPassword: "wrongPassword",
+				username: "betterUsername",
+			});
+		const resp2 = await request(app)
+			.patch("/user/user1")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({
+				email: "wow@mail.com",
+				newPassword: "superSecret",
+				username: "betterUsername",
+			});
+
+		expect(resp.statusCode).toEqual(400);
+		expect(resp2.statusCode).toEqual(400);
+	});
+
+	it("throws an error when updating another user's info", async () => {
+		const u1Token = await GetUserToken();
+		const resp = await request(app)
+			.patch("/user/user2")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({
+				email: "wow@mail.com",
+				newPassword: "superSecret",
+				oldPassword: "password1",
+				username: "betterUsername",
+			});
+
+		expect(resp.statusCode).toBe(403);
 	});
 });
