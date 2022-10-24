2fcc905427b974302f7fa378bbdd87826c04c471
quentinikeno <ikenoq@gmail.com>
2022-10-23 22:06:48 -1000

    Create add method for Friends model.

--- backend/models/friends.js
+++ backend/models/friends.js
@@ -4,11 +4,44 @@ const { NotFoundError404, BadRequestError400 } = require("../expressError");
 const db = require("../db");
 
 class Friends {
-	constructor(id, user_1_id, user_2_id, accepted = false) {
+	constructor(id, user_1_id, user_2_id, accepted) {
 		this.id = id;
 		this.user_1_id = user_1_id;
 		this.user_2_id = user_2_id;
 		this.accepted = accepted;
+	}
+
+	/** add friends
+	 * returns instance of Friends class ({id, user_1_id, user_2_id, accepted})
+	 */
+
+	static async add(user_1_id, user_2_id) {
+		try {
+			// check if users are already friends
+			const friendsResults = await db.query(
+				`
+            SELECT id 
+            FROM friends 
+            WHERE (user_1_id = $1 AND user_2_id = $2) OR (user_2_id = $2 AND user_2_id = $1)
+            `,
+				[user_1_id, user_2_id]
+			);
+
+			if (friendsResults.rows.length)
+				throw new BadRequestError400("Users are already friends.");
+
+			const results = await db.query(
+				`
+			INSERT INTO friends (user_1_id, user_2_id, accepted)
+			VALUES ($1, $2, $3)
+			RETURNING id, user_1_id, user_2_id, accepted`,
+				[user_1_id, user_2_id, false]
+			);
+
+			return new Friends(results.rows[0]);
+		} catch (error) {
+			throw error;
+		}
 	}
 }
 
---
+++ backend/tests/friends.test.js
@@ -0,0 +1,26 @@
+/** Tests for the friends model */
+
+const Friends = require("../models/friends");
+const {
+	commonBeforeAll,
+	commonBeforeEach,
+	commonAfterEach,
+	commonAfterAll,
+	user1,
+	user2,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/** Test Friends model */
+
+describe("test add method", () => {
+	it("can add a friend request for two users to db", async () => {
+		const friends = await Friends.add(user1.id, user2.id);
+
+		expect(friends).toEqual(expect.any(Friends));
+	});
+});
