c947e3f2264a7c22381576f78911d67e170b5ecf
Quentin Ikeno <ikenoq@gmail.com>
2022-10-23 17:11:49 -1000

    Add route to patch user.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,12 @@ const User = require("../models/user");
 const express = require("express");
 const { ensureLoggedIn, ensurePermittedUser } = require("../middleware/auth");
 const { BadRequestError400 } = require("../expressError");
+const validateSchema = require("../helpers/validateSchema");
+const userPatchSchema = require("../schemas/userPatch.json");
 const router = new express.Router();
 
 /** GET user/[username]
- * returns { User{id, username, joinAt, lastLoginAt} }
+ * returns { {id, username, joinAt, lastLoginAt} }
  * authorization: logged in
  */
 
@@ -20,5 +22,33 @@ router.get("/:username", ensureLoggedIn, async (req, res, next) => {
 		return next(error);
 	}
 });
+
+/** PATCH user/[username]
+ * returns { {id, username, joinAt, lastLoginAt} }
+ * authorization: logged in and permitted user
+ */
+
+router.patch(
+	"/:username",
+	ensureLoggedIn,
+	ensurePermittedUser,
+	async (req, res, next) => {
+		try {
+			if (!req.params.username)
+				throw new BadRequestError400("A username must be provided.");
+			validateSchema(req, userPatchSchema);
+
+			const user = await User.authenticate(
+				req.params.username,
+				req.body.oldPassword
+			);
+			const updatedUser = await user.update(req.body);
+
+			return res.json(updatedUser);
+		} catch (error) {
+			return next(error);
+		}
+	}
+);
 
 module.exports = router;
