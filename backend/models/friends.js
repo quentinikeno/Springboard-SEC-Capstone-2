d8809cc2a8bddeb82e271970712bec065071f326
quentinikeno <ikenoq@gmail.com>
2022-10-24 17:26:07 -1000

    Allow constructor to accept an object.

--- backend/models/friends.js
+++ backend/models/friends.js
@@ -4,7 +4,7 @@ const { NotFoundError404, BadRequestError400 } = require("../expressError");
 const db = require("../db");
 
 class Friends {
-	constructor(id, user_1_id, user_2_id, accepted) {
+	constructor({ id, user_1_id, user_2_id, accepted }) {
 		this.id = id;
 		this.user_1_id = user_1_id;
 		this.user_2_id = user_2_id;
