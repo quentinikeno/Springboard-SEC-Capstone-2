4a35eba9ff51d9905cb5946f5e1409d3f97cf172
Quentin Ikeno <ikenoq@gmail.com>
2022-10-25 22:56:15 -1000

    Refactor commonBeforeAll to use promises instead of using an async function.

--- backend/tests/_testCommon.js
+++ backend/tests/_testCommon.js
@@ -5,32 +5,37 @@ const request = require("supertest");
 const app = require("../app");
 const User = require("../models/user");
 
+async function clearTable(table) {
+	await db.query("DELETE FROM $1", [table]);
+}
+
 /** queries and functions to be run before all tests */
-async function commonBeforeAll() {
-	try {
-		await db.query("DELETE FROM friends");
-		await db.query("DELETE FROM users");
 
-		await User.register({
-			username: "user1",
-			email: "user1@test.com",
-			password: "password1",
-		});
-
-		await User.register({
-			username: "user2",
-			email: "user2@test.com",
-			password: "password2",
-		});
-
-		await User.register({
-			username: "user3",
-			email: "user3@test.com",
-			password: "password3",
-		});
-	} catch (error) {
-		console.error(error);
-	}
+function commonBeforeAll() {
+	new Promise(() => db.query("DELETE FROM friends"))
+		.then(() => db.query("DELETE FROM users"))
+		.then(() =>
+			User.register({
+				username: "user1",
+				email: "user1@test.com",
+				password: "password1",
+			})
+		)
+		.then(() =>
+			User.register({
+				username: "user2",
+				email: "user2@test.com",
+				password: "password2",
+			})
+		)
+		.then(() =>
+			User.register({
+				username: "user3",
+				email: "user3@test.com",
+				password: "password3",
+			})
+		)
+		.catch((err) => console.error(err));
 }
 
 async function commonBeforeEach() {
