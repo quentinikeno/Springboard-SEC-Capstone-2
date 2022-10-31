10909944050ef6d21ab53c799ad7ab12d73d46b0
quentinikeno <ikenoq@gmail.com>
2022-10-30 19:40:05 -1000

    Add tests to test patch route.

--- backend/routes/friends.test.js
+++ backend/routes/friends.test.js
@@ -16,7 +16,7 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
-const requestFriends = async () => {
+const requestFriends = async (accept = false) => {
 	const u1Token = await getUserToken();
 	const { body: user2 } = await request(app)
 		.get("/user/user2")
@@ -24,7 +24,13 @@ const requestFriends = async () => {
 	const resp = await request(app)
 		.post(`/friends/${user2.id}`)
 		.set("authorization", `Bearer ${u1Token}`);
-	return { u1Token, user2, resp };
+	let acceptedResp;
+	if (accept) {
+		acceptedResp = await request(app)
+			.patch(`/friends/${user2.id}`)
+			.set("authorization", `Bearer ${u1Token}`);
+	}
+	return { u1Token, user2, resp, acceptedResp };
 };
 
 /** Test Friends routes */
@@ -57,6 +63,11 @@ describe("test GET /friends/[pendingOrAccepted]", () => {
 			.set("authorization", `Bearer ${u1Token}`);
 		expect(resp.statusCode).toBe(400);
 	});
+
+	it("throws an error if the user is not logged in", async () => {
+		const friends = await request(app).get("/friends/pending");
+		expect(friends.statusCode).toBe(401);
+	});
 });
 
 describe("test POST /friends/[userId]", () => {
@@ -75,5 +86,55 @@ describe("test POST /friends/[userId]", () => {
 			.get("/friends/pending")
 			.set("authorization", `Bearer ${u1Token}`);
 		expect(friends.body).toEqual([user2]);
+	});
+
+	it("throws an error if the user is not logged in", async () => {
+		const u1Token = await getUserToken();
+		const { body: user2 } = await request(app)
+			.get("/user/user2")
+			.set("authorization", `Bearer ${u1Token}`);
+		const resp = await request(app).post(`/friends/${user2.id}`);
+		expect(resp.statusCode).toBe(401);
+	});
+});
+
+describe("test PATCH /friends/[userId]", () => {
+	it("accepts a friend request for two users", async () => {
+		const { u1Token, user2, acceptedResp } = await requestFriends(true);
+
+		expect(acceptedResp.statusCode).toBe(201);
+		expect(acceptedResp.body).toEqual({
+			id: expect.any(Number),
+			user_1_id: expect.any(Number),
+			user_2_id: user2.id,
+			accepted: true,
+		});
+
+		const friends = await request(app)
+			.get("/friends/accepted")
+			.set("authorization", `Bearer ${u1Token}`);
+		expect(friends.body).toEqual([user2]);
+	});
+
+	it("throws an error if the two users do not have a pending friend request", async () => {
+		const u1Token = await getUserToken();
+		const { body: user2 } = await request(app)
+			.get("/user/user2")
+			.set("authorization", `Bearer ${u1Token}`);
+
+		const acceptedResp = await request(app)
+			.patch(`/friends/${user2.id}`)
+			.set("authorization", `Bearer ${u1Token}`);
+		expect(acceptedResp.statusCode).toBe(400);
+	});
+
+	it("throws an error if the user is not logged in", async () => {
+		const u1Token = await getUserToken();
+		const { body: user2 } = await request(app)
+			.get("/user/user2")
+			.set("authorization", `Bearer ${u1Token}`);
+
+		const acceptedResp = await request(app).patch(`/friends/${user2.id}`);
+		expect(acceptedResp.statusCode).toBe(401);
 	});
 });
