b04521dc4cb87ff316dab9658970abb4bce9ac02
quentinikeno <ikenoq@gmail.com>
2022-10-31 18:14:03 -1000

    Add method to get games by name.

--- backend/models/games.js
+++ backend/models/games.js
@@ -1,12 +1,40 @@
 /** Games model */
 
-const { BadRequestError400 } = require("../expressError");
+const { BadRequestError400, NotFoundError404 } = require("../expressError");
 const db = require("../db");
 
 class Games {
 	constructor({ id, name }) {
 		this.id = id;
 		this.name = name;
+	}
+
+	/** Gets a game with "name" from the database.
+	 * Returns instance of Game
+	 */
+
+	static async get(name) {
+		try {
+			const results = await db.query(
+				`
+        SELECT id, name
+        FROM games
+        WHERE name=$1
+        `,
+				[name]
+			);
+
+			const game = results.rows[0];
+
+			if (!game)
+				throw new NotFoundError404(
+					`No games found with name: ${name}.`
+				);
+
+			return new Games(game);
+		} catch (error) {
+			throw error;
+		}
 	}
 
 	/** Adds a new game with "name" to the database.
--- backend/models/games.test.js
+++ backend/models/games.test.js
@@ -15,10 +15,26 @@ afterAll(commonAfterAll);
 
 /** Test Games model */
 
+describe("test get games method", () => {
+	it("gets a game with the game's name", async () => {
+		const game = await Games.get("testGame");
+		expect(game).toEqual({ id: expect.any(Number), name: "testGame" });
+	});
+
+	it("throws an error if the game name does not exist", async () => {
+		async () => {
+			await expect(Games.add("nonexistant")).toThrow();
+		};
+	});
+});
+
 describe("test add games method", () => {
 	it("adds a new game to the database", async () => {
 		const game = await Games.add("test");
 		expect(game).toEqual({ id: expect.any(Number), name: "test" });
+
+		const testGame = await Games.get("test");
+		expect(testGame).toEqual(game);
 	});
 
 	it("throws an error if the game name is not unique", async () => {
