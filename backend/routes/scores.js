730b3200a366ec3478283c27345ab4c918608cfa
quentinikeno <ikenoq@gmail.com>
2022-11-05 12:14:43 -1000

    Add PATCH route for /scores.

--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -1,7 +1,6 @@
 const Scores = require("../models/scores");
 const express = require("express");
 const { ensureLoggedIn } = require("../middleware/auth");
-const { BadRequestError400 } = require("../expressError");
 const validateSchema = require("../helpers/validateSchema");
 const scoreAddSchema = require("../schemas/scoreAdd.json");
 const router = new express.Router();
@@ -9,6 +8,11 @@ const router = new express.Router();
 /** All of these routes must ensure that the user is logged in. */
 router.use(ensureLoggedIn);
 
+/** POST /scores
+ * Add a new high score for a logged in user
+ * returns {id, userId, gameId, highScore}
+ * authorization: logged in
+ */
 router.post("/", async (req, res, next) => {
 	try {
 		validateSchema(req, scoreAddSchema);
@@ -19,6 +23,26 @@ router.post("/", async (req, res, next) => {
 		});
 
 		return res.status(201).json(score);
+	} catch (error) {
+		return next(error);
+	}
+});
+
+/** PATCH /scores
+ * Updates a high score for a logged in user
+ * returns {id, userId, gameId, highScore}
+ * authorization: logged in
+ */
+router.patch("/", async (req, res, next) => {
+	try {
+		validateSchema(req, scoreAddSchema);
+
+		const score = await Scores.update({
+			...req.body,
+			userId: res.locals.user.id,
+		});
+
+		return res.json(score);
 	} catch (error) {
 		return next(error);
 	}
--- backend/routes/scores.test.js
+++ backend/routes/scores.test.js
@@ -1,6 +1,5 @@
 /** Tests for the scores routes */
 
-const { expectCt } = require("helmet");
 const request = require("supertest");
 const app = require("../app");
 
@@ -31,6 +30,23 @@ const addScore = async () => {
 	return { u1Token, testGameId, score: resp.body, status: resp.status };
 };
 
+// describe("test POST /scores", () => {
+// 	it("adds a new highscore for a user and game", async () => {
+// 		const { testGameId, score, status } = await addScore();
+// 		expect(status).toBe(201);
+// 		expect(score).toEqual({
+// 			id: expect.any(Number),
+// 			userId: expect.any(Number),
+// 			gameId: testGameId,
+// 			highScore: 1000,
+// 		});
+
+// 		// throws an error if POST again
+// 		const { status: status2 } = await addScore();
+// 		expect(status2).toBe(400);
+// 	});
+// });
+
 describe("test POST /scores", () => {
 	it("adds a new highscore for a user and game", async () => {
 		const { testGameId, score, status } = await addScore();
@@ -41,5 +57,59 @@ describe("test POST /scores", () => {
 			gameId: testGameId,
 			highScore: 1000,
 		});
+
+		// throws an error if POST again
+		const { status: status2 } = await addScore();
+		expect(status2).toBe(400);
+	});
+
+	it("gives 401 status code if the user is not logged in", async () => {
+		const adminToken = await getUserToken("admin", "admin");
+		const gamesResp = await request(app)
+			.get("/games")
+			.set("authorization", `Bearer ${adminToken}`);
+		const testGameId = gamesResp.body.games[0].id;
+		const resp = await request(app)
+			.post("/scores")
+			.send({ gameId: testGameId, highScore: 1000 });
+
+		expect(resp.status).toBe(401);
+	});
+});
+
+describe("test PATCH /scores", () => {
+	it("adds updates a highscore for a user and game", async () => {
+		const { u1Token, testGameId } = await addScore();
+		const resp = await request(app)
+			.patch("/scores")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({ gameId: testGameId, highScore: 2000 });
+
+		expect(resp.body).toEqual({
+			id: expect.any(Number),
+			userId: expect.any(Number),
+			gameId: testGameId,
+			highScore: 2000,
+		});
+	});
+
+	it("returns a 400 status code if the gameId or high score are missing", async () => {
+		const { u1Token, testGameId } = await addScore();
+		const resp = await request(app)
+			.patch("/scores")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({});
+
+		expect(resp.status).toBe(400);
+	});
+
+	it("returns a 400 status code if the high score is a string", async () => {
+		const { u1Token, testGameId } = await addScore();
+		const resp = await request(app)
+			.patch("/scores")
+			.set("authorization", `Bearer ${u1Token}`)
+			.send({ gameId: testGameId, highScore: "score" });
+
+		expect(resp.status).toBe(400);
 	});
 });
