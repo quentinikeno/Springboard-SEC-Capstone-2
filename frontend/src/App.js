45db479df2f174e4f06f75b6572532bad337b191
Quentin Ikeno <ikenoq@gmail.com>
2022-11-12 16:17:20 -1000

    Add a grid to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="grid">
+					<div>One</div>
+					<div>Two</div>
+					<div>Three</div>
+					<div>Four</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
