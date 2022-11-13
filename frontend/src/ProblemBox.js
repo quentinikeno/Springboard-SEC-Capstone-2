0011d6e7feae7d0bee42e6f76d481299712b464b
Quentin Ikeno <ikenoq@gmail.com>
2022-11-12 19:31:26 -1000

    Add form to Problem Box.

--- frontend/src/ProblemBox.js
+++ frontend/src/ProblemBox.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const ProblemBox = ({ prob: { expression, answer } }) => {
+	const [formData, setFormData] = useState({ answer: null });
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormData((formData) => ({ ...formData, [name]: value }));
+	};
+
 	return (
 		<div className="box">
-			{expression} = {answer}
+			<p>{expression}</p>
+
+			<form className="my-5">
+				<div className="field">
+					<label htmlFor="answer" className="label">
+						Label
+					</label>
+					<div className="control">
+						<input
+							className="input"
+							type="number"
+							onChange={handleChange}
+							value={formData.answer}
+							id="answer"
+							name="answer"
+							required
+						/>
+					</div>
+					<div className="control my-3">
+						<button className="button is-primary">Submit</button>
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 };
