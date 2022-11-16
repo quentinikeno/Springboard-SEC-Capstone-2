a8561f33c4eb163343a2aea24e370280557371a1
Quentin Ikeno <ikenoq@gmail.com>
2022-11-15 18:32:43 -1000

    Separate function to determine the max.

--- frontend/src/hooks/useFetchProblems.js
+++ frontend/src/hooks/useFetchProblems.js
@@ -1,7 +1,39 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const useFetchProblems = (operation, min = 0, max = 0) => {
+const determineMax = (operation, i) => {
+	if (operation === "add" || operation === "sub") {
+		if (i < 5) {
+			return 10;
+		} else if (i < 10) {
+			return 15;
+		} else if (i < 15) {
+			return 25;
+		} else if (i < 20) {
+			return 50;
+		} else if (i < 25) {
+			return 75;
+		} else if (i < 30) {
+			return 100;
+		} else if (i < 40) {
+			return 1000;
+		}
+	} else {
+		if (i < 10) {
+			return 5;
+		} else if (i < 20) {
+			return 10;
+		} else if (i < 25) {
+			return 15;
+		} else if (i < 30) {
+			return 20;
+		} else if (i < 40) {
+			return 25;
+		}
+	}
+};
+
+const useFetchProblems = (operation) => {
 	const [problems, setProblems] = useState([]);
 
 	useEffect(function fetchProblemsWhenMounted() {
@@ -9,23 +41,8 @@ const useFetchProblems = (operation, min = 0, max = 0) => {
 			const baseUrl = "https://x-math.herokuapp.com/api";
 			const apiCalls = [];
 			for (let i = 0; i < 40; i++) {
-				if (operation === "add") {
-					if (i < 5) {
-						max = 10;
-					} else if (i < 10) {
-						max = 15;
-					} else if (i < 15) {
-						max = 25;
-					} else if (i < 20) {
-						max = 50;
-					} else if (i < 25) {
-						max = 75;
-					} else if (i < 30) {
-						max = 100;
-					} else if (i < 40) {
-						max = 1000;
-					}
-				}
+				const min = 0;
+				const max = determineMax(operation, i);
 
 				apiCalls.push(
 					axios.get(`${baseUrl}/${operation}/?max=${max}&min=${min}`)
