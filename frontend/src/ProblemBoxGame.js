adbaf6fd83b7c40a1df758384638c883700c670c
Quentin Ikeno <ikenoq@gmail.com>
2022-11-15 18:33:29 -1000

    Create four problem boxes for add, sub, mult, div.

--- frontend/src/ProblemBoxGame.js
+++ frontend/src/ProblemBoxGame.js
@@ -4,23 +4,39 @@ import Loading from "./common/Loading";
 import useFetchProblems from "./hooks/useFetchProblems";
 
 const ProblemBoxGame = () => {
-	const [level, setLevel] = useState(1);
-	const [problems, setProblems] = useFetchProblems("add");
-	const [correct, setCorrect] = useState(false);
+	const [addLevel, setAddLevel] = useState(1);
+	const [addProblems, setAddProblems] = useFetchProblems("add");
+	const [subLevel, setSubLevel] = useState(1);
+	const [subProblems, setSubProblems] = useFetchProblems("sub");
+	const [mulLevel, setMulLevel] = useState(1);
+	const [mulProblems, setMulProblems] = useFetchProblems("mul");
+	const [divLevel, setDivLevel] = useState(1);
+	const [divProblems, setDivProblems] = useFetchProblems("div");
 
-	useEffect(() => {
-		setLevel((level) => level + 1);
-		setCorrect(() => false);
-	}, [correct]);
-
-	if (problems.length === 0) return <Loading />;
+	if (addProblems.length === 0) return <Loading />;
 	return (
-		<ProblemBox
-			level={level}
-			problems={problems}
-			correct={correct}
-			setCorrect={setCorrect}
-		/>
+		<div className="grid">
+			<ProblemBox
+				level={addLevel}
+				setLevel={setAddLevel}
+				problems={addProblems}
+			/>
+			<ProblemBox
+				level={subLevel}
+				setLevel={setSubLevel}
+				problems={subProblems}
+			/>
+			<ProblemBox
+				level={mulLevel}
+				setLevel={setMulLevel}
+				problems={mulProblems}
+			/>
+			<ProblemBox
+				level={divLevel}
+				setLevel={setDivLevel}
+				problems={divProblems}
+			/>
+		</div>
 	);
 };
 
