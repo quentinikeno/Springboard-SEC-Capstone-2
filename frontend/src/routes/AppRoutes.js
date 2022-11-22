bdbc939089d85f681a1216b5a6da6a42220188d8
Quentin Ikeno <ikenoq@gmail.com>
2022-11-21 17:04:14 -1000

    Add route for login.

--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -1,10 +1,12 @@
 import ProblemBoxGame from "../ProblemBoxGame";
+import RegisterUserForm from "../users/RegisterUserForm";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
 	return (
 		<Routes>
 			<Route path={"/"} element={<ProblemBoxGame />} />
+			<Route path={"/auth/login"} element={<RegisterUserForm />} />
 			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
 	);
