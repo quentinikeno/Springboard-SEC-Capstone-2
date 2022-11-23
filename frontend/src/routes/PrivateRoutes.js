28b9693a050809e2d754a30b748dec26dd29994a
Quentin Ikeno <ikenoq@gmail.com>
2022-11-22 18:21:31 -1000

    Remove uneeded dispatch.

--- frontend/src/routes/PrivateRoutes.js
+++ frontend/src/routes/PrivateRoutes.js
@@ -1,10 +1,9 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { selectCurrentUser } from "../redux-slices/auth/authSlice";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const PrivateRoutes = () => {
 	const currentUser = useSelector(selectCurrentUser);
-	const dispatch = useDispatch();
 	return currentUser ? <Outlet /> : <Navigate to="/auth/login" />;
 };
 
