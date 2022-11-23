1f6c321d9db06aa4aeadbcca1209a12dbf2af47c
Quentin Ikeno <ikenoq@gmail.com>
2022-11-22 17:57:34 -1000

    Display username and join date.

--- frontend/src/users/UserProfile.js
+++ frontend/src/users/UserProfile.js
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUser } from "../redux-slices/user/userSlice";
+
 const UserProfile = () => {
-	return <div>UserProfile</div>;
+	const dispatch = useDispatch();
+	const { username, token } = useSelector((state) => state.auth);
+	const { id, joinAt } = useSelector((state) => state.user);
+
+	useEffect(() => {
+		if (!id) {
+			dispatch(getUser({ username, token }));
+		}
+	}, [id]);
+
+	return (
+		<div>
+			<h1>{username}</h1>
+			<p>Joined: {joinAt}</p>
+		</div>
+	);
 };
 
 export default UserProfile;
