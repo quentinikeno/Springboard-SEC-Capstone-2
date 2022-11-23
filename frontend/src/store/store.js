3e382b5a717340afee8fa2a8a1f4740248bc2d26
Quentin Ikeno <ikenoq@gmail.com>
2022-11-22 17:47:01 -1000

    Add the userSlice to store.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../redux-slices/auth/authSlice";
+import userReducer from "../redux-slices/user/userSlice";
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		user: userReducer,
 	},
 });
