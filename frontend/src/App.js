3d505cd20911888954b9124b32ceb231afc1f013
Quentin Ikeno <ikenoq@gmail.com>
2022-11-23 17:23:00 -1000

    Set cookies to persist user info.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./navbar/Navbar";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setCredentials } from "./redux-slices/auth/authSlice";
+import Cookies from "js-cookie";
 import "./App.css";
 
 function App() {
+	const { token, username } = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (token && username) {
+			const attr = { expires: 30, secure: true };
+			Cookies.set("token", token, attr);
+			Cookies.set("username", username, attr);
+		}
+	}, [token]);
+
+	useEffect(() => {
+		const token = Cookies.get("token");
+		const username = Cookies.get("username");
+		dispatch(setCredentials({ token, username }));
+	}, []);
+
 	return (
 		<div className="App container">
 			<Navbar />
