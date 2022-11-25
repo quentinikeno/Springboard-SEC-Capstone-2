a91baf00033839eccf89a20f1a8b2b002a3ad944
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 10:37:19 -1000

    Update tests for authSlice.

--- frontend/src/redux-slices/auth/authSlice.test.js
+++ frontend/src/redux-slices/auth/authSlice.test.js
@@ -1,39 +1,40 @@
 import { store } from "../../store/store";
 import { setCredentials, logOut } from "./authSlice";
 
-const initialState = { token: null, error: null };
-const testUser = {
-	username: "testUser",
-	email: "testUser@example.com",
-	password: "password",
-};
+const initialState = { token: null, username: null, error: null };
 
 it("starts with no tokens in the state", () => {
 	expect(store.getState().auth).toEqual(initialState);
 });
 
 describe("test authSlice reducers", () => {
-	test("setCredentials should set the token in the state", () => {
-		store.dispatch(setCredentials({ token: "testToken" }));
-		expect(store.getState().auth).toEqual({
-			token: "testToken",
-			error: null,
+	describe("test setCredentials", () => {
+		it("sets the token and username in the state", () => {
+			store.dispatch(
+				setCredentials({ token: "testToken", username: "testUser" })
+			);
+			expect(store.getState().auth).toEqual({
+				token: "testToken",
+				username: "testUser",
+				error: null,
+			});
 		});
 	});
 
 	beforeEach(() => {
-		store.dispatch(setCredentials({ token: "testToken" }));
+		store.dispatch(
+			setCredentials({ token: "testToken", username: "testUser" })
+		);
 	});
 
-	test("logOut removes the users token from the state", () => {
-		expect(store.getState().auth).toEqual({
-			token: "testToken",
-			error: null,
-		});
-		store.dispatch(logOut());
-		expect(store.getState().auth).toEqual({
-			token: null,
-			error: null,
+	describe("test logOut", () => {
+		test("removes the users token from the state", () => {
+			store.dispatch(logOut());
+			expect(store.getState().auth).toEqual({
+				token: null,
+				username: null,
+				error: null,
+			});
 		});
 	});
 });
