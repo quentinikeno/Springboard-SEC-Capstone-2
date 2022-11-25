070bdf92a1059b2767d5f347598f83fcdcb33cc6
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 10:48:54 -1000

    Wrap LoginUserForm in Redux Provider and MemoryRouter.

--- frontend/src/users/auth/LoginUserForm.test.js
+++ frontend/src/users/auth/LoginUserForm.test.js
@@ -1,6 +1,15 @@
 import { render } from "@testing-library/react";
 import LoginUserForm from "./LoginUserForm";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import { MemoryRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
-	render(<LoginUserForm />);
+	render(
+		<Provider store={store}>
+			<MemoryRouter>
+				<LoginUserForm />
+			</MemoryRouter>
+		</Provider>
+	);
 });
