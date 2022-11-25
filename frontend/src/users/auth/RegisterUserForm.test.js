89dd360c28c537c85629647015e61fd4ffe10323
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 10:47:27 -1000

    Wrap RegisterUserForm in Redux Provider and MemoryRouter.

--- frontend/src/users/auth/RegisterUserForm.test.js
+++ frontend/src/users/auth/RegisterUserForm.test.js
@@ -1,6 +1,15 @@
 import { render } from "@testing-library/react";
 import RegisterUserForm from "./RegisterUserForm";
+import { store } from "../../store/store";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
-	render(<RegisterUserForm />);
+	render(
+		<Provider store={store}>
+			<MemoryRouter>
+				<RegisterUserForm />
+			</MemoryRouter>
+		</Provider>
+	);
 });
