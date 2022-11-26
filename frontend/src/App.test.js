41483001b92ffefef17f65a33042a87ca6134840
Quentin Ikeno <ikenoq@gmail.com>
2022-11-26 10:35:56 -1000

    Update smoke test for App.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import App from "./App";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+	render(
+		<Provider store={store}>
+			<MemoryRouter>
+				<App />
+			</MemoryRouter>
+		</Provider>
+	);
 });
