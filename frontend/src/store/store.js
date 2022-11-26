3bc5d16889a629565b07915f2ebd311e0ae25880
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 15:49:37 -1000

    Remove authSlice from store.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../redux-slices/auth/authSlice";
 import userReducer from "../redux-slices/user/userSlice";
 
 export const store = configureStore({
 	reducer: {
-		auth: authReducer,
 		user: userReducer,
 	},
 });
