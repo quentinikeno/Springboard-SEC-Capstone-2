58c72a397e47b0c4014dba6b3de2cdb911e82fa0
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 16:24:02 -1000

    Only check for the current user in redux store for Private routes.

--- frontend/src/routes/PrivateRoutes.js
+++ frontend/src/routes/PrivateRoutes.js
@@ -1,12 +1,10 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { selectCurrentUser } from "../redux-slices/user/userSlice";
 import { useSelector } from "react-redux";
-import Cookies from "js-cookie";
 
 const PrivateRoutes = () => {
 	const currentUser = useSelector(selectCurrentUser);
-	const username = Cookies.get("username");
-	return currentUser || username ? <Outlet /> : <Navigate to="/auth/login" />;
+	return currentUser ? <Outlet /> : <Navigate to="/auth/login" />;
 };
 
 export default PrivateRoutes;
