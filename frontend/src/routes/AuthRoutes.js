e8d4265133caba458ae10da2ca3020f13b9abe69
Quentin Ikeno <ikenoq@gmail.com>
2022-11-26 10:50:55 -1000

    Redirect to homepage if no route matches for AuthRoutes.

--- frontend/src/routes/AuthRoutes.js
+++ frontend/src/routes/AuthRoutes.js
@@ -1,12 +1,13 @@
 import LoginUserForm from "../users/auth/LoginUserForm";
 import RegisterUserForm from "../users/auth/RegisterUserForm";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const AuthRoutes = () => {
 	return (
 		<Routes>
 			<Route path={"/register"} element={<RegisterUserForm />} />
 			<Route path={"/login"} element={<LoginUserForm />} />
+			<Route path="*" element={<Navigate to="/" />} />
 		</Routes>
 	);
 };
