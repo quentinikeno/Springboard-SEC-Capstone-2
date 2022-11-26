ec00d1e52a975eaf746d1638e288df240499f755
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 16:52:13 -1000

    Dispatch getUser thunk when user's token and username are in cookies.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,48 @@
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./navbar/Navbar";
+import Loading from "./common/Loading";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setCredentials } from "./redux-slices/user/userSlice";
+import { getUser, setLoading } from "./redux-slices/user/userSlice";
 import Cookies from "js-cookie";
 import "./App.css";
 
 function App() {
-	const { token, username } = useSelector((state) => state.user);
+	const { token, username, loading } = useSelector((state) => state.user);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		if (token && username) {
+		if (token) {
 			const attr = { expires: 30, secure: true };
 			Cookies.set("token", token, attr);
-			Cookies.set("username", username, attr);
 		}
 	}, [token]);
+
+	useEffect(() => {
+		if (username) {
+			const attr = { expires: 30, secure: true };
+			Cookies.set("username", username, attr);
+		}
+	}, [username]);
 
 	useEffect(() => {
 		const token = Cookies.get("token");
 		const username = Cookies.get("username");
 		if (token && username) {
-			dispatch(setCredentials({ token, username }));
+			dispatch(getUser({ token, username }));
 		}
 	}, []);
 
 	return (
 		<div className="App container">
-			<Navbar />
-			<AppRoutes />
+			{loading ? (
+				<Loading />
+			) : (
+				<>
+					<Navbar />
+					<AppRoutes />
+				</>
+			)}
 		</div>
 	);
 }
