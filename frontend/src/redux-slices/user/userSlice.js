a12cd8e5f5f75f7d8413578f19ea1b82bd80ee02
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 15:43:51 -1000

    Combine the auth and user slices.

--- frontend/src/redux-slices/user/userSlice.js
+++ frontend/src/redux-slices/user/userSlice.js
@@ -3,11 +3,56 @@ import axios from "axios";
 
 const apiURL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
+export const registerUser = createAsyncThunk(
+	"user/registerUser",
+	async ({ username, email, password }, { rejectWithValue }) => {
+		try {
+			const resp = await axios.post(`${apiURL}/auth/register`, {
+				username,
+				email,
+				password,
+			});
+			return resp.data; // {token}
+		} catch (error) {
+			return rejectWithValue(error.message);
+		}
+	}
+);
+
+export const loginUser = createAsyncThunk(
+	"user/loginUser",
+	async ({ username, password }, { rejectWithValue }) => {
+		try {
+			const resp = await axios.post(`${apiURL}/auth/login`, {
+				username,
+				password,
+			});
+			return resp.data; // {token}
+		} catch (error) {
+			return rejectWithValue(error.message);
+		}
+	}
+);
+
 export const getUser = createAsyncThunk(
 	"user/getUser",
 	async ({ username, token }, { rejectWithValue }) => {
 		try {
 			const resp = await axios.get(`${apiURL}/user/${username}`, {
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			return resp.data;
+		} catch (error) {
+			return rejectWithValue(error.message);
+		}
+	}
+);
+
+export const updateUser = createAsyncThunk(
+	"user/updateUser",
+	async ({ username, data }, { rejectWithValue }) => {
+		try {
+			const resp = await axios.patch(`${apiURL}/user/${username}`, data, {
 				headers: { Authorization: `Bearer ${token}` },
 			});
 			return resp.data;
@@ -24,6 +69,8 @@ const formatDate = (dateString) => {
 
 const initialState = {
 	id: null,
+	token: null,
+	username: null,
 	joinAt: null,
 	lastLoginAt: null,
 	loading: false,
@@ -31,13 +78,18 @@ const initialState = {
 };
 
 const reducers = {
+	setCredentials: (state, action) => {
+		const { token, username } = action.payload;
+		state.token = token;
+		state.username = username;
+	},
+	logOut: (state, action) => initialState,
 	setUser: (state, action) => {
 		const { id, joinAt, lastLoginAt } = action.payload;
 		state.id = id;
 		state.joinAt = formatDate(joinAt);
 		state.lastLoginAt = formatDate(lastLoginAt);
 	},
-	removeUserOnLogOut: (state, action) => initialState,
 	setLoading: (state, action) => {
 		state.loading = action.payload;
 	},
@@ -49,6 +101,20 @@ const userSlice = createSlice({
 	reducers,
 	extraReducers: (builder) => {
 		builder
+			.addCase(registerUser.fulfilled, (state, action) => {
+				reducers.setCredentials(state, action);
+				state.error = null;
+			})
+			.addCase(registerUser.rejected, (state, action) => {
+				state.error = action.payload;
+			})
+			.addCase(loginUser.fulfilled, (state, action) => {
+				reducers.setCredentials(state, action);
+				state.error = null;
+			})
+			.addCase(loginUser.rejected, (state, action) => {
+				state.error = action.payload;
+			})
 			.addCase(getUser.fulfilled, (state, action) => {
 				reducers.setUser(state, action);
 				state.loading = false;
@@ -56,12 +122,23 @@ const userSlice = createSlice({
 			})
 			.addCase(getUser.rejected, (state, action) => {
 				state.error = action.payload;
+			})
+			.addCase(updateUser.fulfilled, (state, action) => {
+				reducers.updateUser(state, action);
+				state.loading = false;
+				state.error = null;
+			})
+			.addCase(updateUser.rejected, (state, action) => {
+				state.error = action.payload;
 			});
 	},
 });
 
-export const { setUser, removeUserOnLogOut, setLoading } = userSlice.actions;
+export const { setCredentials, logOut, setUser, setLoading } =
+	userSlice.actions;
 
 export default userSlice.reducer;
 
+export const selectCurrentToken = (state) => state.auth.token;
+export const selectCurrentUser = (state) => state.auth.username;
 export const selectUserId = (state) => state.user.id;
--- frontend/src/redux-slices/user/userSlice.test.js
+++ frontend/src/redux-slices/user/userSlice.test.js
@@ -1,7 +1,10 @@
 import { store } from "../../store/store";
-import { setUser, removeUserOnLogOut, setLoading } from "./userSlice";
+import { setCredentials, logOut, setUser, setLoading } from "./userSlice";
 
 const setTestUser = () => {
+	store.dispatch(
+		setCredentials({ token: "testToken", username: "testUser" })
+	);
 	store.dispatch(
 		setUser({
 			id: 1,
@@ -11,46 +14,63 @@ const setTestUser = () => {
 	);
 };
 
-describe("test userSlice reducers", () => {
-	describe("test setUser", () => {
-		it("updates the user info in the store", () => {
-			setTestUser();
-			expect(store.getState().user).toEqual({
-				id: 1,
-				joinAt: "November 21, 2022",
-				lastLoginAt: "November 24, 2022",
-				error: null,
-				loading: false,
-			});
+describe("test setCredentials", () => {
+	it("sets the token and username in the state", () => {
+		store.dispatch(
+			setCredentials({ token: "testToken", username: "testUser" })
+		);
+		expect(store.getState().user).toEqual({
+			token: "testToken",
+			username: "testUser",
+			id: null,
+			joinAt: null,
+			lastLoginAt: null,
+			loading: false,
+			error: null,
 		});
 	});
+});
 
-	beforeEach(() => {
+describe("test logOut", () => {
+	it("removes the users token from the state", () => {
 		setTestUser();
-	});
-
-	describe("test removeUserOnLogOut", () => {
-		it("resets the user info to the intial state", () => {
-			store.dispatch(removeUserOnLogOut());
-			expect(store.getState().user).toEqual({
-				id: null,
-				joinAt: null,
-				lastLoginAt: null,
-				error: null,
-				loading: false,
-			});
+		store.dispatch(logOut());
+		expect(store.getState().user).toEqual({
+			token: null,
+			username: null,
+			id: null,
+			joinAt: null,
+			lastLoginAt: null,
+			loading: false,
+			error: null,
+			error: null,
 		});
 	});
+});
 
-	describe("test setLoading", () => {
-		it("can set loading to true", () => {
-			store.dispatch(setLoading(true));
-			expect(store.getState().user.loading).toBe(true);
+describe("test setUser", () => {
+	it("updates the user info in the store", () => {
+		setTestUser();
+		expect(store.getState().user).toEqual({
+			token: "testToken",
+			username: "testUser",
+			id: 1,
+			joinAt: "November 21, 2022",
+			lastLoginAt: "November 24, 2022",
+			error: null,
+			loading: false,
 		});
+	});
+});
 
-		it("can set loading to false", () => {
-			store.dispatch(setLoading(false));
-			expect(store.getState().user.loading).toBe(false);
-		});
+describe("test setLoading", () => {
+	it("can set loading to true", () => {
+		store.dispatch(setLoading(true));
+		expect(store.getState().user.loading).toBe(true);
+	});
+
+	it("can set loading to false", () => {
+		store.dispatch(setLoading(false));
+		expect(store.getState().user.loading).toBe(false);
 	});
 });
