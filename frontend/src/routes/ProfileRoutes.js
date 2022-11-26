2eb05e97f374a07d1407441a497b174ba3d6040b
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 17:30:41 -1000

    Add route to edit username.

--- frontend/src/routes/ProfileRoutes.js
+++ frontend/src/routes/ProfileRoutes.js
@@ -1,6 +1,7 @@
 import UserProfile from "../users/UserProfile";
 import { Routes, Route } from "react-router-dom";
 import UserDetail from "../users/UserDetail";
+import EditUsernameForm from "../users/EditUsernameForm";
 
 const ProfileRoutes = () => {
 	return (
@@ -8,6 +9,10 @@ const ProfileRoutes = () => {
 			<Route
 				path={"/"}
 				element={<UserProfile child={<UserDetail />} />}
+			/>
+			<Route
+				path={"/edit/username"}
+				element={<UserProfile child={<EditUsernameForm />} />}
 			/>
 		</Routes>
 	);
