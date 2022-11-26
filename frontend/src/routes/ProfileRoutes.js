488bb70e3f977102ddd8b74536df25a452f7d507
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 17:27:32 -1000

    Specify a child component to show for UserProfile component.

--- frontend/src/routes/ProfileRoutes.js
+++ frontend/src/routes/ProfileRoutes.js
@@ -1,10 +1,14 @@
 import UserProfile from "../users/UserProfile";
 import { Routes, Route } from "react-router-dom";
+import UserDetail from "../users/UserDetail";
 
 const ProfileRoutes = () => {
 	return (
 		<Routes>
-			<Route path={"/"} element={<UserProfile />} />
+			<Route
+				path={"/"}
+				element={<UserProfile child={<UserDetail />} />}
+			/>
 		</Routes>
 	);
 };
--- frontend/src/users/UserProfile.js
+++ frontend/src/users/UserProfile.js
@@ -1,12 +1,12 @@
 import UserProfileMenu from "./UserProfileMenu";
 
-const UserProfile = () => {
+const UserProfile = ({ child }) => {
 	return (
 		<div className="columns is-desktop">
 			<div className="column">
 				<UserProfileMenu />
 			</div>
-			<div className="column is-three-quarters"></div>
+			<div className="column is-three-quarters">{child}</div>
 		</div>
 	);
 };
