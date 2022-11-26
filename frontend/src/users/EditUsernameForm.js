d6b93dd30d4abd19da442c9b080d8aef4f1d04d0
Quentin Ikeno <ikenoq@gmail.com>
2022-11-25 15:43:41 -1000

    Create a form to edit username.

---
+++ frontend/src/users/EditUsernameForm.js
@@ -0,0 +1,45 @@
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import useFormState from "../hooks/useFormState";
+
+const EditUsernameForm = () => {
+	const initialState = { username: "" };
+	const [formData, setFormData, handleChange] = useFormState(initialState);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		dispatch();
+		navigate("/profile");
+	};
+	return (
+		<form onSubmit={handleSubmit}>
+			<div className="field">
+				<label htmlFor="username" className="label">
+					New Username
+				</label>
+				<div className="control">
+					<input
+						type="text"
+						className="input"
+						onChange={handleChange}
+						id="username"
+						name="username"
+						value={formData.username}
+						required
+					/>
+				</div>
+			</div>
+			<div className="field is-grouped">
+				<div className="control">
+					<button type="submit" className="button is-link">
+						Change Username
+					</button>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default EditUsernameForm;
