1474ce1611b2c06cbc4698e1480c2fa8becffe38
Quentin Ikeno <ikenoq@gmail.com>
2022-11-27 12:29:28 -1000

    Use UserProfileMenuLink componenets in the menu.

--- frontend/src/users/UserProfileMenu.js
+++ frontend/src/users/UserProfileMenu.js
@@ -1,63 +1,31 @@
 import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../redux-slices/user/userSlice";
+import UserProfileMenuLink from "./UserProfileMenuLink";
 
 const UserProfileMenu = () => {
-	const location = useLocation();
 	const username = useSelector(selectCurrentUser);
+
 	return (
 		<aside className="menu">
 			<p className="menu-label">Your Profile</p>
 			<ul className="menu-list">
-				<li>
-					<Link
-						className={`${
-							location.pathname === "/profile" ? "is-active" : ""
-						}`}
-						to="/profile"
-					>
-						{username}
-					</Link>
-				</li>
+				<UserProfileMenuLink to={"/profile"} text={username} />
 			</ul>
 			<p className="menu-label">Edit Your Info</p>
 			<ul className="menu-list">
-				<li>
-					<Link
-						className={`${
-							location.pathname === "/profile/edit/username"
-								? "is-active"
-								: ""
-						}`}
-						to="/profile/edit/username"
-					>
-						Username
-					</Link>
-				</li>
-				<li>
-					<Link
-						className={`${
-							location.pathname === "/profile/edit/email"
-								? "is-active"
-								: ""
-						}`}
-						to="/profile/edit/email"
-					>
-						Email
-					</Link>
-				</li>
-				<li>
-					<Link
-						className={`${
-							location.pathname === "/profile/edit/password"
-								? "is-active"
-								: ""
-						}`}
-						to="/profile/edit/password"
-					>
-						Password
-					</Link>
-				</li>
+				<UserProfileMenuLink
+					to={"/profile/edit/username"}
+					text={"Username"}
+				/>
+				<UserProfileMenuLink
+					to={"/profile/edit/email"}
+					text={"Email"}
+				/>
+				<UserProfileMenuLink
+					to={"/profile/edit/password"}
+					text={"Password"}
+				/>
 			</ul>
 		</aside>
 	);
