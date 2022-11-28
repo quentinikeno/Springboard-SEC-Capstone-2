16d7fad9b5134cd6bb65609a4b7e682f0fc142a5
Quentin Ikeno <ikenoq@gmail.com>
2022-11-27 23:21:42 -1000

    Add tests for getMathProblem.

--- frontend/src/helpers/genMathProblem.test.js
+++ frontend/src/helpers/genMathProblem.test.js
@@ -1,4 +1,4 @@
-import { randNum, findDivisor } from "./genMathProblem";
+import { randNum, findDivisor, getMathProblem } from "./genMathProblem";
 
 describe("test randNum", () => {
 	it("should generate a random number up to two digits by default", () => {
@@ -33,7 +33,7 @@ describe("test findDivisor", () => {
 	it("should generate a random divisor between two numbers", () => {
 		const result = findDivisor(27, 8, 2);
 		const { first, second, answer } = result;
-		console.log(result);
+
 		expect(result).toEqual({
 			first: expect.any(Number),
 			second: expect.any(Number),
@@ -41,5 +41,77 @@ describe("test findDivisor", () => {
 		});
 		expect(first / second).toBe(answer);
 		expect(first % second).toBe(0);
+	});
+});
+
+describe("test getMathProblem", () => {
+	it("should generate a random addition problem", () => {
+		const result = getMathProblem("add");
+
+		expect(result).toEqual({
+			first: expect.any(Number),
+			second: expect.any(Number),
+			operation: "&plus;",
+			expression: `${result.first} ${result.operation} ${result.second}`,
+			answer: result.first + result.second,
+		});
+	});
+
+	it("should generate a random subtraction problem", () => {
+		const result = getMathProblem("sub");
+
+		expect(result).toEqual({
+			first: expect.any(Number),
+			second: expect.any(Number),
+			operation: "&minus;",
+			expression: `${result.first} ${result.operation} ${result.second}`,
+			answer: result.first - result.second,
+		});
+		expect(result.answer).toBeGreaterThan(0);
+	});
+
+	it("should generate a random subtraction problem with negative answers", () => {
+		const result = getMathProblem("sub", 2, true);
+
+		expect(result).toEqual({
+			first: expect.any(Number),
+			second: expect.any(Number),
+			operation: "&minus;",
+			expression: `${result.first} ${result.operation} ${result.second}`,
+			answer: result.first - result.second,
+		});
+	});
+
+	it("should generate a random multiplication problem", () => {
+		const result = getMathProblem("mul");
+
+		expect(result).toEqual({
+			first: expect.any(Number),
+			second: expect.any(Number),
+			operation: "&times;",
+			expression: `${result.first} ${result.operation} ${result.second}`,
+			answer: result.first * result.second,
+		});
+	});
+
+	it("should generate a random division problem", () => {
+		const result = getMathProblem("div");
+
+		expect(result).toEqual({
+			first: expect.any(Number),
+			second: expect.any(Number),
+			operation: "&divide;",
+			expression: `${result.first} ${result.operation} ${result.second}`,
+			answer: result.first / result.second,
+		});
+	});
+
+	it("should throw an error if the type is not add, sub, mul, or div", () => {
+		const setBadType = () => {
+			getMathProblem("bad");
+		};
+		expect(setBadType).toThrow(
+			new Error("Type must be 'add', 'sub', 'mul', or 'div'.")
+		);
 	});
 });
