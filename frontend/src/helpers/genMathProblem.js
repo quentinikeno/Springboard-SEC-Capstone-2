2557b7cf4263f40f5c88222b36e8eab355908390
Quentin Ikeno <ikenoq@gmail.com>
2022-11-27 22:48:58 -1000

    Create findDivisor function.

--- frontend/src/helpers/genMathProblem.js
+++ frontend/src/helpers/genMathProblem.js
@@ -1,6 +1,21 @@
 export const randNum = (digits = 2) =>
 	Math.floor(Math.random() * Math.pow(10, digits)) + 1;
 
+export const findDivisor = (first, second, digits) => {
+	if (first % second === 0) return { first, second, answer: first / second };
+
+	const divisors = [];
+
+	for (let i = 1; i < first / 2; i++) {
+		if (first % i === 0) divisors.push(i);
+	}
+
+	const randIndex = Math.floor(Math.random() * divisors.length);
+	second =
+		Math.random() < 0.5 ? divisors[randIndex] : first / divisors[randIndex];
+	return { first: first, second, answer: first / second };
+};
+
 export const getMathProblem = (type, digits = 2, allowNegative = false) => {
 	let first = randNum(digits);
 	let second = randNum(digits);
--- frontend/src/helpers/genMathProblem.test.js
+++ frontend/src/helpers/genMathProblem.test.js
@@ -1,4 +1,4 @@
-import { randNum } from "./genMathProblem";
+import { randNum, findDivisor } from "./genMathProblem";
 
 describe("test randNum", () => {
 	it("should generate a random number up to two digits by default", () => {
@@ -19,5 +19,27 @@ describe("test randNum", () => {
 	it("should return 1 if 0 passed as param", () => {
 		const num = randNum(0);
 		expect(num).toEqual(1);
+	});
+});
+
+describe("test findDivisor", () => {
+	it("should return the first and second numbers as is, along with the answer if the second is a divisor of the first", () => {
+		expect(findDivisor(10, 5, 2)).toEqual({
+			first: 10,
+			second: 5,
+			answer: 2,
+		});
+	});
+	it("should generate a random divisor between two numbers", () => {
+		const result = findDivisor(27, 8, 2);
+		const { first, second, answer } = result;
+		console.log(result);
+		expect(result).toEqual({
+			first: expect.any(Number),
+			second: expect.any(Number),
+			answer: expect.any(Number),
+		});
+		expect(first / second).toBe(answer);
+		expect(first % second).toBe(0);
 	});
 });
