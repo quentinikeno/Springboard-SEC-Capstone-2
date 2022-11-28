2143fb0b1edbec8896845dda1638608f9ad5860f
Quentin Ikeno <ikenoq@gmail.com>
2022-11-27 21:42:23 -1000

    Return an object with an multiplication problem.

--- frontend/src/helpers/genMathProblem.js
+++ frontend/src/helpers/genMathProblem.js
@@ -1,7 +1,7 @@
 const randNum = (digits = 2) =>
 	Math.floor(Math.random() * Math.pow(10, digits)) + 1;
 
-export const getMathProblem = (type, digits = 2) => {
+export const getMathProblem = (type, digits = 2, allowNegative = false) => {
 	let first = randNum(digits);
 	let second = randNum(digits);
 
@@ -9,6 +9,19 @@ export const getMathProblem = (type, digits = 2) => {
 		const operation = "+";
 		const expression = `${first} ${operation} ${second}`;
 		const answer = first + second;
+		return { first, second, operation, expression, answer };
+	} else if (type === "sub") {
+		if (first - second < 0 && !allowNegative) {
+			[first, second] = [second, first];
+		}
+		const operation = "-";
+		const expression = `${first} ${operation} ${second}`;
+		const answer = first - second;
+		return { first, second, operation, expression, answer };
+	} else if (type === "mult") {
+		const operation = "x";
+		const expression = `${first} ${operation} ${second}`;
+		const answer = first * second;
 		return { first, second, operation, expression, answer };
 	}
 };
