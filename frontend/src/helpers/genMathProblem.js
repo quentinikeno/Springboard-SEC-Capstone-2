a70098d0cf57fd10850944dae3051834536e1f13
Quentin Ikeno <ikenoq@gmail.com>
2022-11-27 21:33:47 -1000

    Return an object with an addition problem.

--- frontend/src/helpers/genMathProblem.js
+++ frontend/src/helpers/genMathProblem.js
@@ -2,5 +2,13 @@ const randNum = (digits = 2) =>
 	Math.floor(Math.random() * Math.pow(10, digits)) + 1;
 
 export const getMathProblem = (type, digits = 2) => {
-	const answerObj = {};
+	let first = randNum(digits);
+	let second = randNum(digits);
+
+	if (type === "add") {
+		const operation = "+";
+		const expression = `${first} ${operation} ${second}`;
+		const answer = first + second;
+		return { first, second, operation, expression, answer };
+	}
 };
