d6eda079e04165c8b180822720f7521276aa08c8
Quentin Ikeno <ikenoq@gmail.com>
2022-11-28 19:24:51 -1000

    Add problemBoxes reducer to store.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux-slices/user/userSlice";
+import problemBoxesReducer from "../redux-slices/problemBoxes";
 
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
+		problemBoxes: problemBoxesReducer,
 	},
 });
