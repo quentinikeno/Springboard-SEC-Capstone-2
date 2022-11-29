e6788cf9630552eb253f8a58fbf8987dda7af10d
Quentin Ikeno <ikenoq@gmail.com>
2022-11-28 19:47:09 -1000

    Remove state and props for problems and levels.

--- frontend/src/ProblemBoxGame.js
+++ frontend/src/ProblemBoxGame.js
@@ -1,16 +1,9 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import ProblemBoxes from "./ProblemBoxes";
-import useFetchProblems from "./hooks/useFetchProblems";
 import { decrementSeconds } from "./redux-slices/problemBoxes/problemBoxesSlice";
 
 const ProblemBoxGame = () => {
-	const [level, setLevel] = useState({ add: 1, sub: 1, mul: 1, div: 1 });
-	const [addProblems, setAddProblems] = useFetchProblems("add");
-	const [subProblems, setSubProblems] = useFetchProblems("sub");
-	const [mulProblems, setMulProblems] = useFetchProblems("mul");
-	const [divProblems, setDivProblems] = useFetchProblems("div");
-
 	const timerId = useRef();
 	const { seconds } = useSelector((state) => state.problemBoxes);
 	const dispatch = useDispatch();
@@ -34,26 +27,11 @@ const ProblemBoxGame = () => {
 		return min + ":" + `${sec}`.padStart(2, "0");
 	};
 
-	const calcScore = (levels) =>
-		Object.values(levels).reduce((sum, nextLevel) => sum + nextLevel, 0) -
-		4;
-
 	return (
 		<div className="ProblemBoxGame">
 			<h2>{convertToMinutesSeconds(seconds)}</h2>
 
-			{seconds > 0 ? (
-				<ProblemBoxes
-					level={level}
-					setLevel={setLevel}
-					addProblems={addProblems}
-					subProblems={subProblems}
-					mulProblems={mulProblems}
-					divProblems={divProblems}
-				/>
-			) : (
-				<div>Time's up! Your score is {calcScore(level)}.</div>
-			)}
+			{seconds > 0 ? <ProblemBoxes /> : <div>Time's up!.</div>}
 		</div>
 	);
 };
