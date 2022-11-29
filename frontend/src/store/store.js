bbfcf57f22362d25796e57f45b88d846011ce510
Quentin Ikeno <ikenoq@gmail.com>
2022-11-28 19:27:02 -1000

    Import from the problemBoxesSlice

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux-slices/user/userSlice";
-import problemBoxesReducer from "../redux-slices/problemBoxes";
+import problemBoxesReducer from "../redux-slices/problemBoxes/problemBoxesSlice";
 
 export const store = configureStore({
 	reducer: {
