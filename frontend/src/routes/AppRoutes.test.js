3028c542abf0a9cd9683f01ec662ca4e9f4859bd
Quentin Ikeno <ikenoq@gmail.com>
2022-11-29 16:51:57 -1000

    Wrap AppRoutes with Redux store Provider.

--- frontend/src/routes/AppRoutes.test.js
+++ frontend/src/routes/AppRoutes.test.js
@@ -1,11 +1,15 @@
 import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "../store/store";
 import { MemoryRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 
 it("renders without crashing", () => {
 	render(
-		<MemoryRouter>
-			<AppRoutes />
-		</MemoryRouter>
+		<Provider store={store}>
+			<MemoryRouter>
+				<AppRoutes />
+			</MemoryRouter>
+		</Provider>
 	);
 });
