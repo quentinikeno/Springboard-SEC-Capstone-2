85337e8882a9b3bd5ce30b026efaea95fa6ef5f2
Quentin Ikeno <ikenoq@gmail.com>
2022-12-05 17:59:35 -1000

    Add MemoryRouter to FlashMessages test.

--- frontend/src/flash-messages/FlashMessages.test.js
+++ frontend/src/flash-messages/FlashMessages.test.js
@@ -2,11 +2,14 @@ import { render } from "@testing-library/react";
 import FlashMessages from "./FlashMessages";
 import { store } from "../store/store";
 import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
 	render(
 		<Provider store={store}>
-			<FlashMessages />
+			<MemoryRouter>
+				<FlashMessages />
+			</MemoryRouter>
 		</Provider>
 	);
 });
