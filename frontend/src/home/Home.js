dc2616559c4c1c041ff3ff3b04aa572204568142
Quentin Ikeno <ikenoq@gmail.com>
2022-12-07 17:53:25 -1000

    Add route for Home page.

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,11 +1,13 @@
 const Home = () => {
 	return (
-		<div className="hero-body">
-			<div className="container has-text-centered">
-				<p className="title">Title</p>
-				<p className="subtitle">Subtitle</p>
+		<section className="hero is-fullheight">
+			<div className="hero-body">
+				<div className="container has-text-centered">
+					<p className="title">Title</p>
+					<p className="subtitle">Subtitle</p>
+				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
--- frontend/src/routes/AppRoutes.js
+++ frontend/src/routes/AppRoutes.js
@@ -2,12 +2,14 @@ import ProblemBoxGame from "../math-squared/ProblemBoxGame";
 import AuthRoutes from "./AuthRoutes";
 import ProfileRoutes from "./ProfileRoutes";
 import PrivateRoutes from "./PrivateRoutes";
+import Home from "../home/Home";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
 	return (
 		<Routes>
-			<Route path={"/"} element={<ProblemBoxGame />} />
+			<Route path={"/"} element={<Home />} />
+			<Route path={"/game"} element={<ProblemBoxGame />} />
 			<Route path={"/auth/*"} element={<AuthRoutes />} />
 			<Route element={<PrivateRoutes />}>
 				<Route path={"/profile/*"} element={<ProfileRoutes />} />
