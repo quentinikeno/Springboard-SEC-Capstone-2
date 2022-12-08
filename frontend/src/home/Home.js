b2d62c78d30966b092d05dc701cf71cb1c59d0e6
Quentin Ikeno <ikenoq@gmail.com>
2022-12-07 17:48:34 -1000

    Create home page component.

---
+++ frontend/src/home/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+	return (
+		<div className="hero-body">
+			<div className="container has-text-centered">
+				<p className="title">Title</p>
+				<p className="subtitle">Subtitle</p>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
---
+++ frontend/src/home/Home.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import Home from "./Home";
+
+it("renders without crashing", () => {
+	render(<Home />);
+});
