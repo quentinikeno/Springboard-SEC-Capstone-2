5c5f205e4a285a7c34d51162245908820c500938
Quentin Ikeno <ikenoq@gmail.com>
2022-12-07 18:25:38 -1000

    Add content for home page.

--- frontend/src/home/Home.js
+++ frontend/src/home/Home.js
@@ -1,13 +1,52 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
 	return (
-		<section className="hero is-fullheight">
-			<div className="hero-body">
-				<div className="container has-text-centered">
-					<p className="title">Title</p>
-					<p className="subtitle">Subtitle</p>
+		<div className="has-text-centered">
+			<section className="hero">
+				<div className="hero-body">
+					<div className="container">
+						<h1 className="title">Math Squared</h1>
+						<p className="subtitle">Practice your artihmetic!</p>
+
+						<Link to="/game">
+							<div className="button is-primary is-large">
+								Start!
+							</div>
+						</Link>
+					</div>
 				</div>
+			</section>
+
+			<hr />
+
+			<div className="columns is-desktop">
+				<section className="my-6 column">
+					<p>
+						Solve addtion, subtraction, multiplication, and division
+						problems that get harder as you progress. You'll start
+						by solving addition problems and as you complete more
+						you will be able to start solving subtraction problems.
+						Then complete subtraction problems to start working on
+						multiplication problems and finally complete
+						multiplication problems to start working on division
+						problems until you see problem squares for all four
+						operations.
+					</p>
+				</section>
+				<section className="my-6 column">
+					<p>Login/Sign Up to save your score.</p>
+					<div className="buttons is-justify-content-center my-4">
+						<Link to="/auth/login">
+							<div className="button is-info mr-3">Login</div>
+						</Link>
+						<Link to="/auth/register">
+							<div className="button is-link">Register</div>
+						</Link>
+					</div>
+				</section>
 			</div>
-		</section>
+		</div>
 	);
 };
 
