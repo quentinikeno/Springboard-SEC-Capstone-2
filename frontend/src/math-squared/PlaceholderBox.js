69ac63f5b8bca2fa199f8b9786746507a6ec4cd9
Quentin Ikeno <ikenoq@gmail.com>
2022-12-08 18:30:28 -1000

    Create a placholder box.

---
+++ frontend/src/math-squared/PlaceholderBox.js
@@ -0,0 +1,7 @@
+const PlacholderBox = ({operation}) => {
+  return (
+    <div className="column"><div className="box">operation</div></div>
+  )
+}
+
+export default PlacholderBox
---
+++ frontend/src/math-squared/PlaceholderBox.test.js
@@ -0,0 +1,6 @@
+import { render } from "@testing-library/react";
+import PlacholderBox from "./PlaceholderBox";
+
+it('renders without crashing', ()=>{
+    render(<PlacholderBox/>)
+})
