4e518e417fd25982e7ff404c7396dff6f86a4213
Quentin Ikeno <ikenoq@gmail.com>
2022-12-08 18:21:46 -1000

    Wrap Home in MemoryRouter.

--- frontend/src/home/Home.test.js
+++ frontend/src/home/Home.test.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
+import {MemoryRouter} from 'react-router-dom'
 import Home from "./Home";
 
 it("renders without crashing", () => {
-	render(<Home />);
+	render(<MemoryRouter><Home /></MemoryRouter>);
 });
