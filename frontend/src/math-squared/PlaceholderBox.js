487612998c5423aba71cd82a9db48034eda3702f
Quentin Ikeno <ikenoq@gmail.com>
2022-12-08 19:05:24 -1000

    Display progress to unlock in Placholder boxes.

--- frontend/src/math-squared/PlaceholderBox.js
+++ frontend/src/math-squared/PlaceholderBox.js
@@ -1,7 +1,16 @@
-const PlaceholderBox = ({iconClass}) => {
-  return (
-    <div className="column"><div className="box"><i class={`fa-solid ${iconClass}`}></i></div></div>
-  )
-}
+const PlaceholderBox = ({ iconClass, progress }) => {
+	return (
+		<div className="column has-text-centered">
+			<div className="box">
+				<i className={`fa-solid ${iconClass} fa-xl`}></i>
+				<progress className="progress my-5" value={progress} max="100">
+					{progress}
+				</progress>
+				<p className="my-5">Progress to Unlock: {progress}%</p>
+				<i className="fa-solid fa-lock fa-2xl"></i>
+			</div>
+		</div>
+	);
+};
 
-export default PlaceholderBox
+export default PlaceholderBox;
--- frontend/src/math-squared/ProblemBoxes.js
+++ frontend/src/math-squared/ProblemBoxes.js
@@ -4,15 +4,44 @@ import { useSelector } from "react-redux";
 
 const ProblemBoxes = () => {
 	const { level } = useSelector((state) => state.mathSquared);
+	const { add: addLevel, sub: subLevel, mul: mulLevel } = level;
+	const numToBeat = 5;
 	return (
 		<>
 			<div className="columns">
-				<ProblemBox operation={"add"} iconClass={"fa-plus"}/>
-				{level["add"] > 5 ? <ProblemBox operation={"sub"} iconClass={"fa-minus"}/> : <PlacholderBox iconClass={"fa-minus"} />}
+				<ProblemBox operation={"add"} iconClass={"fa-plus"} />
+				{addLevel > numToBeat ? (
+					<ProblemBox operation={"sub"} iconClass={"fa-minus"} />
+				) : (
+					<PlacholderBox
+						iconClass={"fa-minus"}
+						progress={Math.round(
+							(100 * (addLevel - 1)) / numToBeat
+						)}
+					/>
+				)}
 			</div>
 			<div className="columns">
-				{level["sub"] > 5 ? <ProblemBox operation={"mul"} iconClass={"fa-xmark"} /> : <PlacholderBox iconClass={"fa-xmark"} />}
-				{level["mul"] > 5 ? <ProblemBox operation={"div"} iconClass={"fa-divide"}/> : <PlacholderBox iconClass={"fa-divide"} />}
+				{subLevel > numToBeat ? (
+					<ProblemBox operation={"mul"} iconClass={"fa-xmark"} />
+				) : (
+					<PlacholderBox
+						iconClass={"fa-xmark"}
+						progress={Math.round(
+							(100 * (subLevel - 1)) / numToBeat
+						)}
+					/>
+				)}
+				{mulLevel > numToBeat ? (
+					<ProblemBox operation={"div"} iconClass={"fa-divide"} />
+				) : (
+					<PlacholderBox
+						iconClass={"fa-divide"}
+						progress={Math.round(
+							(100 * (mulLevel - 1)) / numToBeat
+						)}
+					/>
+				)}
 			</div>
 		</>
 	);
