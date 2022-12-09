1ad0d7021c6d755e2775e127315d28a0ec7ecee5
Quentin Ikeno <ikenoq@gmail.com>
2022-12-08 19:09:12 -1000

    Set progress bar to info color.

--- frontend/src/math-squared/PlaceholderBox.js
+++ frontend/src/math-squared/PlaceholderBox.js
@@ -3,7 +3,11 @@ const PlaceholderBox = ({ iconClass, progress }) => {
 		<div className="column has-text-centered">
 			<div className="box">
 				<i className={`fa-solid ${iconClass} fa-xl`}></i>
-				<progress className="progress my-5" value={progress} max="100">
+				<progress
+					className="progress my-5 is-info"
+					value={progress}
+					max="100"
+				>
 					{progress}
 				</progress>
 				<p className="my-5">Progress to Unlock: {progress}%</p>
