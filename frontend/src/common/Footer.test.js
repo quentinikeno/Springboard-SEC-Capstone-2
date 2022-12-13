785ae7843dee72b5e68b539e5a87228316bcddd0
Quentin Ikeno <ikenoq@gmail.com>
2022-12-12 17:27:31 -1000

    Wrap footer in store provider and memory router.

--- frontend/src/common/Footer.test.js
+++ frontend/src/common/Footer.test.js
@@ -1,6 +1,15 @@
 import { render } from "@testing-library/react";
 import Footer from "./Footer";
+import { store } from "../store/store";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
 
 it("renders without crashing", () => {
-	render(<Footer />);
+	render(
+		<Provider store={store}>
+			<MemoryRouter>
+				<Footer />
+			</MemoryRouter>
+		</Provider>
+	);
 });
