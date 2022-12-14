e263bbad8940655dffcc1b22cea1d31058c04bae
Quentin Ikeno <ikenoq@gmail.com>
2022-12-13 18:32:39 -1000

    Display correct port number.

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const { PORT } = require("./config");
 
 app.listen(PORT, () => {
-	console.log(`Server starting on port 5000`);
+	console.log(`Server starting on port ${PORT}`);
 });
