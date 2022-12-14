298211869cea2ef9d980d604569e9efe723ff322
Quentin Ikeno <ikenoq@gmail.com>
2022-12-13 18:27:04 -1000

    Console log the config varaiables.

--- backend/config.js
+++ backend/config.js
@@ -15,6 +15,12 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
 const BCRYPT_WORK_FACTOR = 12;
 
+console.log("Config:");
+console.log("SECRET_KEY:", SECRET_KEY);
+console.log("BCRYPT_WORK_FACTOR", BCRYPT_WORK_FACTOR);
+console.log("Database:", DB_URI);
+console.log("---");
+
 module.exports = {
 	DB_URI,
 	SECRET_KEY,
